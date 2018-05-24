2ed4040460ba3411525fd7767c28e0f6c2b9114e
uno <st9007a@gmail.com>
2018-05-24 10:39:10 +0800

    Ball Beam system

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+from math import sin, cos, pi
 
 class PID():
 
@@ -9,46 +10,105 @@ class PID():
         self.ki = ki
         self.kd = kd
 
+        self.sample_time = 0.01
+        self.curr_ts = time.time()
+        self.prev_ts = self.curr_ts
+
+        self.init()
+
     def init(self):
 
-        self.prev_ts = time.time()
-        self.current_ts = self.prev_ts
+        self.set_point = 0.0
 
-        self.p = 0
-        self.i = 0
-        self.d = 0
+        self.p = 0.0
+        self.i = 0.0
+        self.d = 0.0
 
         self.last_err = 0
         self.err = 0
 
-    def compute(self, answer, feedback):
+        self.windup_guard = 20.0
+        self.output = 0.0
 
-        self.err = answer - feedback
-        self.current_ts = time.time()
+    def compute(self, feedback):
 
-        self.p = self.err
-        self.i += (self.current_ts - self.prev_ts) * self.err
-        self.d = self.err - self.last_err
+        self.err = self.set_point - feedback
+        self.curr_ts = time.time()
 
-        self.prev_ts = self.current_ts
+        delta_time = self.curr_ts - self.prev_ts
+        delta_err = self.err - self.last_err
+
+
+        if delta_time >= self.sample_time:
+            self.p = self.err
+            self.i += delta_time * self.err
+
+            if self.i > self.windup_guard:
+                self.i = self.windup_guard
+            elif self.i < -self.windup_guard:
+                self.i = -self.windup_guard
+
+            self.d = delta_err / delta_time
+
+        self.prev_ts = self.curr_ts
         self.last_err = self.err
 
-        return self.kp * self.p + self.ki * self.i + self.kd * self.d
+        self.output = self.kp * self.p + self.ki * self.i + self.kd * self.d
 
-def y(t):
-    return 0 if t < 100 else 1
+class BallBeam():
+
+    def __init__(self, r, theta, a):
+
+        self.a = a
+
+        self.x1 = r
+        self.x2 = 0.0
+        self.x3 = theta
+        self.x4 = 0.0
+
+        self.init()
+
+    def init(self):
+
+        self.B = 0.6
+        self.G = 9.8
+
+        self.curr_ts = time.time()
+        self.start_ts = self.curr_ts
+        self.prev_ts = self.curr_ts
+
+    def feed(self, u):
+
+        x1_dot = self.x2
+        x2_dot = self.B * (self.x1 * self.x4 * self.x4 - self.G * sin(self.x3))
+        x3_dot = self.x4
+        x4_dot = u
+
+        self.curr_ts = time.time()
+        delta_time = self.curr_ts - self.prev_ts
+
+        self.x1 += x1_dot * delta_time
+        self.x2 += x2_dot * delta_time
+        self.x3 += x3_dot * delta_time
+        self.x4 += x4_dot * delta_time
+
+        self.prev_ts = self.curr_ts
+        self.state = self.x1
+
+    def yd(self):
+        return self.a * cos(pi * (time.time() - self.start_ts) / 5)
+
 
 if __name__ == '__main__':
 
-    controller = PID(kp = 0.8, ki = 3, kd = 0.001)
-    controller.init()
-    feedback = 0
-
-    i = 1
+    pid = PID(kp = 1.2, ki = 1, kd = 0.001)
+    bb = BallBeam(r = 1, theta = 0.0564, a = 1)
 
     while True:
-        feedback = controller.compute(y(i), feedback)
-        print(controller.err, feedback)
+        pid.set_point = bb.yd()
+        bb.feed(pid.output)
+        pid.compute(bb.state)
 
         time.sleep(0.02)
-        i += 1
+
+        print(bb.x1, bb.x2, bb.x3, bb.x4, pid.set_point)
