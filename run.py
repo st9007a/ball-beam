f5d1f142f7c26c10073f6183f821aad7fdabe946
uno <st9007a@gmail.com>
2018-05-28 14:05:00 +0800

    Change parameter

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import time
 from math import pi
 
 from opt.bench import Bench
-from opt.PSO import StandardPSO
+from opt.PSO import PSO_CW
 
 from ballbeam import PID, BallBeam
 
@@ -17,18 +17,19 @@ def expr(r, theta, a, kp, ki, kd):
         bb.feed(pid.output)
         pid.compute(bb.output)
 
-        if abs(bb.output) > 4 or (bb.total_time) > 15:
+        if abs(bb.output) > 4 or (bb.total_time) > 30:
             break
 
     return pid.objective_value / pid.total_time
 
 def objective(vec):
-    return expr(1, 0.0564, 1, vec[0], vec[1], vec[2])
+    return expr(2, 0.1129, 1, vec[0], vec[1], vec[2])
 
 if __name__ == '__main__':
 
-    optimizer = StandardPSO(c1 = 0.5, c2 = 0.5, num_particles = 100)
-    bench_fn = Bench(dims = 3, func = objective, up = 10, low = 1e-9, optima = 0)
+    optimizer = PSO_CW(c1 = 2, c2 = 2.1, num_particles = 100)
+    bench_fn = Bench(dims = 3, func = objective, up = 10, low = 0, optima = 0)
 
     optimizer.optimize(iters = 10000, bench = bench_fn)
+    # optimizer.optimize(iters = 1000000, bench = bench_fn, set_vec = [0.764178799, 0.277689114, 0.0])
     print(optimizer.best_vec)
