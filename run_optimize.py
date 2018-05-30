88e43818da9cd6039c95c923e227233b19a83034
uno <st9007a@gmail.com>
2018-05-30 17:58:22 +0800

    Rename script

--- run_optimize.py
+++ run_optimize.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import time
+import sys
 from math import pi
 
 from opt.bench import Bench
@@ -7,9 +8,15 @@ from opt.PSO import PSO_CW
 
 from ballbeam import PID, BallBeam
 
-A = 3
-R = 3
-THETA = 0.1698
+if len(sys.argv) <= 1:
+    print('Usage: python3 run_optimize.py [A] [R] [THETA]')
+    print('')
+    print('Example: python3 run_optimize.py 1 1 0.0564')
+    exit()
+
+A = int(sys.argv[1])
+R = int(sys.argv[2])
+THETA = float(sys.argv[3])
 
 def expr(r, theta, a, kp, ki, kd):
 
@@ -32,7 +39,7 @@ def objective(vec):
 if __name__ == '__main__':
 
     optimizer = PSO_CW(c1 = 2, c2 = 2.1, num_particles = 100)
-    bench_fn = Bench(dims = 3, func = objective, up = 0.5, low = 0, optima = 0)
+    bench_fn = Bench(dims = 3, func = objective, up = 1, low = 0, optima = 0)
 
     optimizer.optimize(iters = 10000, bench = bench_fn)
     print(optimizer.best_vec)
